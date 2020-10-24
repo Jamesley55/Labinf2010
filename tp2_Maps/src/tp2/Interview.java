98dcfb86d3a411aefbd297375eca57231d99f0d9
unknown <alex.hua@polymtl.ca>
2020-10-24 17:40:28 -0400

    plus que 7 test a faire part 2

--- tp2_Maps/src/tp2/Interview.java
+++ tp2_Maps/src/tp2/Interview.java
@@ -12,6 +12,34 @@ public final class Interview {
      */
     //  Character.getNumericValue()
     public static boolean isValidFlipped(String listOfNumbers) {
+        char[] array = new char[listOfNumbers.length()];
+        char[] flippedArray = new char[listOfNumbers.length()];
+
+        for(int i = 0; i < listOfNumbers.length(); i++) {
+            array[i] = listOfNumbers.charAt(i);
+        }
+
+        for(int i = 0; i < listOfNumbers.length(); i++){
+            if(array[i] == '3' || array[i] == '4' || array[i] == '7')
+                return false;
+            else if(array[i] == '0')
+                flippedArray[listOfNumbers.length() - i] = '0';
+            else if(array[i] == '1')
+                flippedArray[listOfNumbers.length() - i] = '1';
+            else if(array[i] == '2')
+                flippedArray[listOfNumbers.length() - i] = '2';
+            else if(array[i] == '5')
+                flippedArray[listOfNumbers.length() - i] = '5';
+            else if(array[i] == '6')
+                flippedArray[listOfNumbers.length() - i] = '9';
+            else if(array[i] == '8')
+                flippedArray[listOfNumbers.length() - i] = '8';
+            else if(array[i] == '9')
+                flippedArray[listOfNumbers.length() - i] = '6';
+        }
+
+        if(java.util.Arrays.equals(array, flippedArray) == true)
+            return true;
         return false;
     }
 
