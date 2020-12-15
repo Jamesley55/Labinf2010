f8684464995927a1667338089c5985d61ad189e2
jamesley55 <Jamesleyjoseph5@gmail.com>
2020-12-15 14:56:48 -0500

    lab 5

--- tp5_Graphs/src/main/java/KthSmallestElement/KthSmallestElement.java
+++ tp5_Graphs/src/main/java/KthSmallestElement/KthSmallestElement.java
@@ -4,7 +4,10 @@ import java.util.PriorityQueue;
 public class KthSmallestElement {
     /**
      * Explication de votre complexité temporelle
-     *
+     * tout d'abord, nous avons mis tout les elements de la premiere colonne de la matrice dans un priorityQueue
+     * le PriorityQueue a une complexite temporelle log(n) pour l'appelle a la fonction "add"
+     * le "for loop" sur la premiere colonne et la fonction "add" du PriorityQueue
+     * nous donne une complexite temporelle O(cLog(c)).
      *
      * Explication de votre complexité spatiale
      *
@@ -22,71 +25,42 @@ public class KthSmallestElement {
      * @return `K`th smallest element in `matrix`, null if non-existent
      */
     static public <T extends Comparable<T>> T findKthSmallestElement(final T[][] matrix, final int k) {
-
-
-       if(matrix == null || k > matrix.length*matrix[0].length-1){
+       // c = column , r = row
+       // Matrix c*r
+       if(matrix == null || k > matrix.length*matrix[0].length-1 || k <0){
            return null;
        }
+        PriorityQueue<Node> pq = new PriorityQueue<>();
 
-      /* Node[] heap = new Node[matrix.length];
-       for(int row =0; row <matrix.length;row++){
-           heap[row] = new Node(0,row,matrix[row][0]);
+        // 0(c*log(c))
+        for(int i =0; i< matrix.length; i++){
+           pq.add(new Node(0,i, (Integer) matrix[i][0]));
        }
-       return null;
 
-       */
+        // O(k*log(c))
+        for(int i=0; i<k; i++){
 
-        int ke = k+1;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                pq.offer((Integer) matrix[i][j]);
+            // enleve la plus petite valeur
+            // et on le met dans une variable temporaire
+            // O(log(c))
+            Node headNode = pq.poll();
+
+            // quand il y a une valeur a gauche
+            // on le rajoute dans le pq
+            if(headNode.x < matrix[headNode.y].length-1){
+                // O(log(c+1))
+                pq.add(new Node(headNode.x+1, headNode.y, (Integer) matrix[headNode.y][headNode.x+1]));
             }
+            // complexite temporelle max total
+            // O(c*log(c))+ O(k*log(c))
+            // O((c+k)log(c))
+
         }
-        while (--ke > 0) {
-            pq.poll();
-        }
-        return (T) pq.peek();
-
-
-
-       /*  PriorityQueue<Node>pq=new PriorityQueue<>();
-        int n=matrix.length;
-        for(int i=0;i<n;i++)
-            pq.add(new Node(0, i, (Integer) matrix[0][i]));
-
-        Node node = new Node(0,0, null);
-
-
-        for(int j=1;j<k;j++)
-        {
-            node =pq.poll();
-            T nextValue = node.row < matrix.length - 1 ?
-                    matrix[node.collum+1][node.row] : null;
-
-
-            pq.add(new Node(node.collum+1, node.row, nextValue));
-        }
-
         return (T) pq.poll().value;
 
-        */
     }
 
 
-
-    static void swap(int i, int min, Node[] arr)
-    {
-        Node temp = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp;
-    }
-
-    static void heapify(Node[] heap, int index){
-        int RightChild = 2*index +2;
-        int leftChild = 2*index +1;
-
-    }
 }
 
 
@@ -94,27 +68,22 @@ public class KthSmallestElement {
 
 
  class Node implements Comparable{
-    int collum;
-    int row;
-    Comparable value;
+    int x;
+    int y;
+    Integer value;
 
-    public Node(int collum, int row, Comparable value){
-        this.collum = collum;
-        this.row = row;
+    public Node(int x, int y, Integer value){
+        this.x = x;
+        this.y = y;
         this.value = value;
     }
 
     @Override
     public int compareTo(Object object) {
         if (!(object instanceof Node)) {
-            throw new IllegalArgumentException("Object o is not of type Pixel");
+            throw new IllegalArgumentException("Object o is not of type Node");
         }
-        Node node = (Node) object;
-        if(this.value == null )
-            return -1;
-        if(node.value == null )
-            return 1;
-        return node.value.compareTo(this.value);
+         return this.value - ((Node) object).value;
     }
 
 }
